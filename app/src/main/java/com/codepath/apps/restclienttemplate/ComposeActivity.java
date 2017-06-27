a64a4367b3a2b27d89c64ef36ba8034c21499d1d
Samer Kadamani <samerk@fb.com>
2017-06-27 16:24:20 -0700

    Added character counter

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -19,6 +23,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class ComposeActivity extends AppCompatActivity {
     private final int REQUEST_CODE = 20;
+    private TextView sms_count;
     EditText etCompose;
     Tweet tweet;
 
@@ -27,7 +32,26 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        sms_count = (TextView) findViewById(R.id.tvChar);
         etCompose = (EditText) findViewById(R.id.etCompose);
+
+        final TextWatcher txwatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                sms_count.setText(String.valueOf(140 - s.length()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        etCompose.addTextChangedListener(txwatcher);
     }
 
     public void onSubmit(View v) {
